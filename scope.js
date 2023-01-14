02660ef449e023c5f46f52f6113487d4b9e768af
Rakib Hassan <mailrakibhassan@gmail.com>
2023-01-14 18:49:23 +0600

    scope done

--- scope.js
+++ scope.js
@@ -1,2 +1,22 @@
-var x = 23;
-console.log(x); // 23
+// parent doesn't have access of its children's scope
+// var x = 23; // variable in global scope
+// console.log(x); // 23
+// console.log(window.x); // 23 => x variable is in window object
+
+
+function myFunc() {
+    // var y = 10; // variable in function scope can't be accessed by parent scope
+
+
+
+    // x = 10; // reassignment of a global variable. but using var before this will make it a function scoped and will not change the global variable
+
+
+    // var x = 23;
+    // child has access to the parent scopes
+    // console.log(`${x} from myFunc`); // 23
+}
+
+// myFunc();
+
+// console.log(x); // 10;
